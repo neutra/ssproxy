ad6aadbb77f1a3203392cd4c61508517304e352c
neutra <neutra@qq.com>
2016-12-30 09:00:08 +0800

    add debug trace

--- server.js
+++ server.js
@@ -10,43 +10,66 @@
 
 //ssserver.run(config);
 
-var http=require('http');
-var https=require('https');
-var url=require('url');
+var http = require('http');
+var https = require('https');
+var url = require('url');
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.createServer(function(req,res){
-    var body='';
-    req.on('data',function(chunk){
-            body+=chunk;
+var DEBUG_TRACE = true;
+
+var log = {
+	i: function(msg) {
+		console.log(msg);
+	},
+	d: function(msg) {
+		if(DEBUG_TRACE) console.log(msg);
+	}
+};
+
+http.createServer(function(req, res) {
+    var body = '';
+    req.on('data', function(chunk) {
+    	body += chunk;
     });
-    req.on('end',function(){
-			console.log("> "+req.url);
+    req.on('end', function() {
+        var request_url = req.url;
+        if(!request_url) {
+        	return res.end('must have url!');
+        } else {
+			log.i("> " + request_url);
+        }
+		log.d("headers:");
+		log.d(req.headers);
 
-            if(!req.url) return res.end('must have url!');
-            var request_url=req.url;
-            var option=url.parse(request_url);
-            var httpObj='http:'==option.protocol?http:https;
+        var option = url.parse(request_url);
 
-            req.headers.host=option.host;
-            option.path=option.path?option.path:option.pathname+(option.search?option.search:'');//cloudfrondry上 url.parse 后 没有path这个下标
-            req.headers.path=option.path;
-            delete req.headers.fetchurl;
-            option.method=req.method;
-            option.headers=req.headers;
-            httpObj.request(option,function(result){
-                for(var key in result.headers){
-                    res.setHeader(key,result.headers[key]);
-                }
-                result.on('data',function(chunk){
-                    res.write(chunk);
-                });
-                result.on('end',function(){
-                    res.end();
-                });
-            }).on('error',function(error){
-                res.end('remote http.request error'+error)}).end(body);
+		log.d("url parse result:");
+		log.d(option);
 
-    });
-}).listen(port);
-console.log("listen: "+port);
+        req.headers.host = option.host;
+        option.path = option.path ? option.path : option.pathname + (option.search ? option.search : '');
+        req.headers.path = option.path;
+        option.method = req.method;
+        option.headers = req.headers;
+
+		log.d("proxy options:");
+		log.d(option);
+
+        var httpObj = 'http:' == option.protocol ? http : https;
+        httpObj.request(option, function(result) {
+            for(var key in result.headers) {
+                res.setHeader(key, result.headers[key]);
+            }
+            result.on('data', function(chunk) {
+                res.write(chunk);
+            });
+            result.on('end', function() {
+                res.end();
+            });
+        }).on('error', function(error) {
+            res.end('remote http.request error ' + error)}).end(body);
+
+    	});
+	}).listen(port);
+
+log.i("listen: " + port);
