bc2cc46032104c8ee27f8b4f2940a495d3d4fbb3
neutra <neutra@qq.com>
2016-12-30 09:57:02 +0800

    add local agent

--- local.js
+++ local.js
@@ -1,21 +1,9 @@
-//var config = {};
-
-//config.server = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-//config.server_port = process.env.OPENSHIFT_NODEJS_PORT || 1080;
-//config.password = process.env.OPENSHIFT_NODEJS_PASSWD || "123456";
-//config.timeout = process.env.OPENSHIFT_NODEJS_TIMEO || 300;
-//config.method = process.env.OPENSHIFT_NODEJS_IP || "aes-128-cfb";
-
-//var ssserver = require("./ssserver");
-
-//ssserver.run(config);
-
 var http = require('http');
-var https = require('https');
 var url = require('url');
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = 8088;
 
 var DEBUG_TRACE = true;
+var SERVER_HOST = "http://127.0.0.1:8080/";
 
 var log = {
 	i: function(msg) {
@@ -32,23 +20,18 @@ http.createServer(function(req, res) {
     	body += chunk;
     });
     req.on('end', function() {
-        var targeturl = req.headers.targeturl;
-        if(!targeturl) return res.send("must have targeturl");
+        var targeturl = req.url;
 		log.i("> " + targeturl);
 
-		log.d("headers:");
-		log.d(req.headers);
-
-        var option = url.parse(targeturl);
+        var option = url.parse(SERVER_HOST);
         option.method = req.method;
-        delete req.headers.targeturl;
         option.headers = req.headers;
+        option.headers.targeturl = targeturl;
 
 		log.d("proxy options:");
 		log.d(option);
 
-        var httpObj = 'http:' == option.protocol ? http : https;
-        httpObj.request(option, function(result) {
+        http.request(option, function(result) {
             for(var key in result.headers) {
                 res.setHeader(key, result.headers[key]);
             }
